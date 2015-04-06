d81c56cab7b016477890cb97d6b5cd2b47122acb
quylaa <quylaa@gmail.com>
2015-04-06 12:04:21 -0700

    lab 8

---
+++ lab8/Student_Code/Factory.cpp
@@ -0,0 +1,17 @@
+#include "Factory.h"
+/*
+	You will MODIFY THIS DOCUMENT.
+*/
+//=======================================================================================
+/*
+	getQS()
+
+	Creates and returns an object whose class extends QSInterface.
+	This should be an object of a class you have created.
+
+	Example: If you made a class called "QS", you might say, "return new QS();".
+*/
+QSInterface * Factory::getQS()
+{
+  return NULL;//Modify this line
+}
